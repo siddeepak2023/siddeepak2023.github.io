f6afdeff45ef2ef023f8b946d3877eb827657671
John Doe <johndoe@example.com>
2021-10-01 11:54:02 -0500

    ini

--- Java/MonsterFighter/src/MonsterFighter.java
+++ Java/MonsterFighter/src/MonsterFighter.java
@@ -1,5 +1,59 @@
-public class App {
+public class MonsterFighter {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Monster Player = new Monster("Peekandchew");
+        Monster Player = new Monster("Charsalad");
+
+        int roundcounter = 0
+        while (player.isAlive() && Computer.isAlive()) {
+            if(roundcounter % 2 == 0) Player
+        }
     }
-}
+} 
+
+Peekandchew attacks!!
+Chardsalad takes 1 damage!
+Chardsalad attacks!!
+Peekandchew takes 1 damage!
+Peekandchew attacks!!
+Chardsalad takes 1 damage!
+Chardsalad attacks!!
+Peekandchew blocks the hit!
+Peekandchew attacks!!
+Chardsalad blocks the hit!
+Chardsalad attacks!!
+Peekandchew takes 2 damage!
+Peekandchew attacks!!
+Chardsalad blocks the hit!
+Chardsalad attacks!!
+Peekandchew blocks the hit!
+Peekandchew attacks!!
+Chardsalad blocks the hit!
+Chardsalad attacks!!
+Peekandchew blocks the hit!
+Peekandchew attacks!!
+Chardsalad takes 1 damage!
+Chardsalad attacks!!
+Peekandchew takes 1 damage!
+Peekandchew attacks!!
+Chardsalad takes 1 damage!
+Chardsalad attacks!!
+Peekandchew blocks the hit!
+Peekandchew attacks!!
+Chardsalad takes 1 damage!
+Chardsalad attacks!!
+Peekandchew takes 1 damage!
+Peekandchew attacks!!
+Chardsalad takes 1 damage!
+Chardsalad attacks!!
+Peekandchew takes 1 damage!
+Peekandchew attacks!!
+Chardsalad takes 2 damage!
+Chardsalad attacks!!
+Peekandchew takes 1 damage!
+Peekandchew attacks!!
+Chardsalad takes 1 damage!
+Chardsalad attacks!!
+Peekandchew takes 2 damage!
+Peekandchew attacks!!
+Chardsalad takes 1 damage!
+Chardsalad falls to the ground!
